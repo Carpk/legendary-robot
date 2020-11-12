c87b9df7e67222e393532971536fba0bfc8f8348
Shawn <zappeln@gmail.com>
2020-11-12 14:00:23 -0600

    Create orderManagement.h

---
+++ orderManagement.h
@@ -0,0 +1,15 @@
+void doAdd (order **head);
+
+void doCallAhead (order **head);
+
+void doWaiting (order *head);
+
+void doRetrieve (order **head);
+
+void doList (order *head);
+
+void doDisplay (order *head);
+
+void doEstimateTime(order *head);
+
+
